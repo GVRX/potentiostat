983f94e547e2f835c02376483ec16672225a0b6d
GVRX <g.vanriessen@latrobe.edu.au>
2021-03-19 20:21:11 +1100

    Replace setup.py to support installation of ECL specific interface and apps.

--- software/python/potentiostat/setup.py
+++ software/python/potentiostat/setup.py
@@ -2,48 +2,9 @@
 iorodeo-potentiostat
 ---------------------
 
-Python interface to IO Rodeo's Potentiostat Shield for the teensy 3.2 development 
-board.
+Python interface to LTU Electrocheminiluminescence(ECL)/Potentiometer  Shield for the teensy 3.6 development 
+board.  Based upon the IO Rodeostat potentiometer (Will Dickson, http://stuff.iorodeo.com/docs/potentiostat).
 
-Example
---------
-
-.. code:: python
-
-    from potentiostat import Potentiostat
-
-    dev = Potentiostat('/dev/ttyACM0')
-    dev.set_curr_range('100uA')
-    dev.set_sample_period(10)
-    
-    name = 'cyclic'
-    param = {
-            'quietValue' : 0.0,
-            'quietTime'  : 1000,
-            'amplitude'  : 2.0,
-            'offset'     : 0.0,
-            'period'     : 1000,
-            'numCycles'  : 5,
-            'shift'      : 0.0,
-            }
-    
-    dev.set_param(name,param)
-    t,volt,curr = dev.run_test(name,display='pbar')
-
-
-Install
---------
-
-.. code:: bash
-
-    $ pip install iorodeo-potentiostat
-
-
-Links
------
-
-* Documentation http://stuff.iorodeo.com/docs/potentiostat
-* Download https://bitbucket.org/iorodeo/potentiostat
 
 """
 from setuptools import setup, find_packages
@@ -52,13 +13,13 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name='iorodeo-potentiostat',
-    version='0.0.3',
-    description='Serial interface to IO Rodeo Potentiostat',
+    name='eclometer',
+    version='0.0.1',
+    description='ECLometer serial interface, CLI and GUI app.',
     long_description=__doc__,
     url='https://bitbucket.org/iorodeo/potentiostat',
-    author='Will Dickson',
-    author_email='will@iorodeo.com',
+    author='Grant van Riessen  et al.',
+    author_email='g.vanriessen@latrobe.edu.au',
     license='MIT',
 
     classifiers=[
@@ -69,24 +30,32 @@ setup(
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',#added gvr
+        'Programming Language :: Python :: 3.8',#added gvr
+        'Programming Language :: Python :: 3.9',#added gvr
         'Operating System :: POSIX', 
         'Operating System :: Microsoft :: Windows', 
         'Operating System :: MacOS :: MacOS X',
     ],
 
-    keywords='Serial interface for IO Rodeo Potentiostat',
-    #packages=find_packages(exclude=['docs', 'tests', 'examples']),
+    keywords='ECL ECLometer potentiostat' ,
     packages=find_packages(exclude=['docs', 'tests']),
 
     install_requires=['pyserial',
                       'progressbar33',
                       'argparse',
-                      'json-python-module',
-                      'json_tricks',
+                      'json-python-module',  #added gvr
+                      'json_tricks',    #added gvr
+                      'drawnow',  #added gvr
+                      'matplotlib',  #added gvr
+                      'numpy',  #added gvr
+                      'gooey',  #added gvr
+                      'pylab',  #added gvr
+                      'matplotlib'  #added gvr
                       ],
 )
